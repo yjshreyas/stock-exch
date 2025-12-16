96f60817a357f0214aee5bbbee3431eda517276f
yjshreyas <yjshreyas@gmail.com>
2025-12-16 11:27:05 +0530

    Update db.js

--- db.js
+++ db.js
@@ -20,6 +20,37 @@
 
 
 
+// // db.js
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const connectDB = async () => {
+//     const MONGO_URI = process.env.MONGO_URI;
+
+//     if (!MONGO_URI) {
+//         console.error("❌ ERROR: MONGO_URI is NOT set in environment variables!");
+//         process.exit(1);
+//     }
+
+//     try {
+//         await mongoose.connect(MONGO_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+
+//         console.log("✅ MongoDB connected successfully.");
+//     } catch (err) {
+//         console.error("❌ MongoDB connection failed:", err.message);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
+
+
+
 // db.js
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
@@ -39,7 +70,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-
+        // await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB connected successfully.");
     } catch (err) {
         console.error("❌ MongoDB connection failed:", err.message);
